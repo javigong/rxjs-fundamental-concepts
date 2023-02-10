e81903198f143391673cde9b3bf7725da5b257db
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-02-09 18:12:55 -0800

    The of and from operators

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { fromEvent, interval, Observable, timer } from "rxjs";
+import { from, fromEvent, interval, Observable, of, timer } from "rxjs";
 
-// Create an observable operator from scratch
+//* Create an observable from scratch
 // const observable = new Observable(
 //   (observer) => {
 //     observer.next('Hello');
@@ -9,25 +9,46 @@ import { fromEvent, interval, Observable, timer } from "rxjs";
 //   }
 // );
 
-// Subscribe an observer to the observable operator
+//* Subscribe an observer to the observable from scratch
 // const subscription = observable.subscribe(
 //   (value) => console.log(value),
 //   (error) => console.log(error),
 //   () => console.log('Completed')
 // );
 
-// Create an observable operator interval with 1 second interval
+//* Operators are functions that take an observable as input and return a new observable as output
+
+//* CREATION OPERATORS: 
+//* Create an observable from scratch or from other sources
+
+//* Create an observable operator interval with 1 second interval
 // const observable = interval(1000);
 
-// Create an observable operator timer with 5 seconds delay and 2 seconds interval
+//* Create an observable operator timer with 5 seconds delay and 2 seconds interval
 // const observable = timer(5000, 2000);
 
-// Create an observable operator fromEvent with document click event
-const observable = fromEvent(
-  document, 'click'
-)
+//* Create an observable operator fromEvent with document click event
+// const observable = fromEvent(
+//   document, 'click'
+// )
 
-// Subscribe an observer to the observable operator
-const subscription = observable.subscribe(
-  console.log
+//* Create an of operator with 5 values
+// const observable = of([1, 2, 3, 4, 5]);
+
+//* Create a from operator with a fetch request to an API endpoint and return the response as an observable
+const observable = from(
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
 );
+
+//* Subscribe an observer to the observable operator
+const subscription = observable.subscribe({
+  next(value) {
+    console.log(value)
+  },
+  complete() {
+    console.log('Completed')
+  }
+}
+);
+
+console.log('hello')
